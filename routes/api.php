d50bceaef96c5deaba93dddb5128062c6d652d58
MatteoFilippini <matteofilippini8@gmail.com>
2022-04-04 15:49:14 +0200

    created controller and route api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// davanti abbiamo sempre api/
+Route::get('/posts', 'ApiController@index', function () {
+    return response()->json('posts');
+});
